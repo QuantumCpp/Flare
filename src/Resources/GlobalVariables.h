b59da12bc1bc72818ad1520a7aede7d63a0a4a3f
QuantumCpp <jeremynobel16@gmail.com>
2026-01-23 15:40:05 -0500

    refactor(parser): separate tokenization and validation, improve project structure
    - Extract tokenization and validation into dedicated headers
    - Add new token types (OptionGroup, Separation)
    - Improve tokenization logic for long, short and grouped options
    - Reorganize project layout (src / Resources)

---
+++ src/Resources/GlobalVariables.h
@@ -0,0 +1,52 @@
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+#include <filesystem> 
+
+//Comandos:
+//1.Listar archivos : Fecha de creacion - Nombre - Extension - Tamano 
+
+enum class TypeToken{
+  Command,
+  Option,
+  Separation,
+  OptionGroup,
+  Positional
+};
+
+enum class ValuePolicy{
+  None,
+  Required,
+  Optional
+};
+
+enum class ValidationError{
+  AllCorrect,
+  CommandIncorrectPosition,
+  CommandNotFound,
+  OptionNotFound,
+  OptionRequiredValue,
+  OptionNotRequiredValue
+} ;
+
+struct Token{
+  TypeToken type;
+  std::string name;
+  std::string value; //solo si es una OptionWithValue
+};
+
+const std::unordered_map<std::string,ValuePolicy> CommandSpec = {
+    {"list" , ValuePolicy::None}
+  };
+const std::unordered_map<std::string,ValuePolicy> OptionSpec = {
+    {"--extension", ValuePolicy::Required},
+    {"-e" , ValuePolicy::None},
+    {"-s" , ValuePolicy::None},
+    {"-d" , ValuePolicy::None},
+
+  };
+
+bool StartWith(const std::string& arg, const std::string& start){
+  return !arg.empty() && arg.find(start) == 0;
+}
---
+++ src/Resources/validation.h
@@ -0,0 +1,45 @@
+#include "GlobalVariables.h"
+
+ValidationError Validation(std::vector<Token>& tokens){
+  
+  for (size_t i = 0 ; i < tokens.size() ; i ++){
+    Token token = tokens[i];
+    
+    if (i == 0){
+      if(tokens[i].type != TypeToken::Command){
+        return ValidationError::CommandIncorrectPosition;
+      }
+    }
+
+    //Validar que un comando existe : 
+    if (token.type == TypeToken::Command){
+      if (CommandSpec.find(token.name) != CommandSpec.end()){
+        continue;
+      }
+      else{
+        return ValidationError::CommandNotFound;
+      }
+    }
+    
+    if(token.type == TypeToken::Option){
+      if (OptionSpec.find(token.name)  != OptionSpec.end()){
+        if((OptionSpec.at(token.name) == ValuePolicy::None && token.value == "") ||  
+            (OptionSpec.at(token.name) == ValuePolicy::Required && token.value != ""))
+        {
+          continue;
+        }
+
+         if (OptionSpec.at(token.name) == ValuePolicy::None && token.value != "") {return ValidationError::OptionNotRequiredValue; } 
+         if (OptionSpec.at(token.name) == ValuePolicy::Required && token.value == "") {return ValidationError::OptionRequiredValue; }
+      }
+      else{
+        return ValidationError::OptionNotFound;
+      }
+    }
+
+  }
+
+
+  return ValidationError::AllCorrect;
+}
+
