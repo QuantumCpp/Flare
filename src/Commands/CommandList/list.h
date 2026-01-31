0844c95e93ac550e2ada7d6f82bb94a5713dc885
QuantumCpp <jeremynobel16@gmail.com>
2026-01-31 16:50:51 -0500

    Basic implementation of the list command in the event that no options have been entered + error handling for cases where the folder does not exist and in the event that no folder entered is valid.

--- src/Commands/CommandList/list.h
+++ src/Commands/CommandList/list.h
@@ -1,2 +1,8 @@
 #pragma once
+#include "../../SystemUtilites/GeneralLibraries/GeneralLibraries.h"
+#include "../../SystemUtilites/Structs/Token.h"
+#include "../../SystemUtilites/Structs/TokenGroup.h"
+#include "../../SystemUtilites/EnumsClass/ValidationError.h"
+
+ValidationError CommandList(const TokenGroup& TokenGroupRaw);
 
