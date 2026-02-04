61d72a4bc3086901aadd381b3863cf3b69b2ddb6
QuantumCpp <jeremynobel16@gmail.com>
2026-02-04 09:15:24 -0500

    Style : Commit pre-style change to C++ 20

--- src/commands/list/list.cpp
+++ src/commands/list/list.cpp
@@ -9,6 +9,9 @@
 #include <iostream>
 #include "../../system/types/DataError.h"
 #include "../../system/registry/error/error_registry.h"
+#include "../../system/types/Token.h"
+#include "../../system/types/TokenGroup.h"
+#include "../../system/types/ValidationError.h"
 
 void OrderedDirectoryItems(std::vector<std::filesystem::directory_entry>& FilesDirectory){
   std::sort(FilesDirectory.begin(), FilesDirectory.end(), 
@@ -30,13 +33,13 @@ bool CommandList(const TokenGroup &TokenGroupRaw){
   std::filesystem::path path = std::filesystem::current_path();  
   const DataErrorDetail* ErrorSucess = nullptr;
 
-  //Guardar los elementos option del TokenGroupRaw en un unordered_set
-  for(const auto& file : TokenOption){
-    OptionFind.insert(file.name);
+  OptionFind.reserve(TokenOption.size());
+  for(const auto& token : TokenOption){
+    OptionFind.insert(token.name);
   }
 
-  bool recursive = OptionFind.count("--recursive") ? true : false;
-  bool LongFormat = OptionFind.count("--long") ? true : false;
+  bool recursive = OptionFind.contains("--recursive");
+
 
   std::vector<std::string> DirectoryName;
   std::unordered_map<std::string,std::vector<std::filesystem::directory_entry>> FilePath;
@@ -45,17 +48,28 @@ bool CommandList(const TokenGroup &TokenGroupRaw){
   if(!TokenPositional.empty()){
     //Guardar y verificar que directorios en positional son validos
     for(const auto& Directory : TokenPositional){
-      path = Directory.name;
-
+      std::filesystem::path path = Directory.name;
+      
       if(std::filesystem::exists(path) && std::filesystem::is_directory(path)){
         DirectoryName.emplace_back(Directory.name);
         std::vector<std::filesystem::directory_entry> FilesDirectory;
+        
+        //Si la opcion recursiva esta disponible sera usarada
+        if(recursive){
+          for(const auto& file : std::filesystem::recursive_directory_iterator(path)){
+            FilesDirectory.push_back(file);
+          }
 
-        for(const auto& file : std::filesystem::directory_iterator(path)){
-          FilesDirectory.push_back(file);
         }
-        OrderedDirectoryItems(FilesDirectory);
-        FilePath[Directory.name] = FilesDirectory;
+        else{
+          for(const auto& file : std::filesystem::directory_iterator(path)){
+            FilesDirectory.push_back(file);
+          }
+        }
+        if(!FilesDirectory.empty()){
+          OrderedDirectoryItems(FilesDirectory);
+          FilePath[Directory.name] = std::move(FilesDirectory);
+        }
       }
       else{
         ErrorSucess = GetError(ValidationError::PathDoesNotExist);
@@ -69,8 +83,10 @@ bool CommandList(const TokenGroup &TokenGroupRaw){
     for(const auto& file : std::filesystem::directory_iterator(path)){
       FilesDirectory.emplace_back(file);
     }
-    OrderedDirectoryItems(FilesDirectory);
-    FilePath[DirectoryName.front()] = FilesDirectory;
+    if(!FilesDirectory.empty()){
+      OrderedDirectoryItems(FilesDirectory);
+      FilePath[DirectoryName.front()] = std::move(FilesDirectory);
+    }
   }
 
 
